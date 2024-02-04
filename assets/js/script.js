0930d6c59b813eaa067651261deb67ba39c79985
Silvia Saverino <silviasave89@gmail.com>
2024-02-04 23:55:13 +0000

    store the deck_id of the drawn card into the variable cardId

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,12 @@
 document.getElementById("get-card-btn").addEventListener("click", handleClickOnGetCardBtn)
 
+let cardId
+
 function handleClickOnGetCardBtn() {
     fetch("https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/")
         .then(res => res.json())
-        .then(data => console.log(data))
+        .then(data => {
+            console.log(data)
+            cardId = data.deck_id
+        })
 }
