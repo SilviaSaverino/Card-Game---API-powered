d7fce00dfd49a2f3078fc0fe99985bb1ccbfb719
Silvia Saverino <silviasave89@gmail.com>
2024-02-04 22:30:27 +0000

    initial setup

---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+document.getElementById("get-card-btn").addEventListener("click", function() {
+    fetch("https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/")
+        .then(res => res.json())
+        .then(data => console.log(data))
+})
