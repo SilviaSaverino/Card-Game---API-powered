b54356c0a8e6dda38deef4307580f7ea1a9de376
Silvia Saverino <silviasave89@gmail.com>
2024-02-09 17:38:10 +0000

    add functionality to determine the winner card, based on their index value

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,7 +11,7 @@ function handleClickOnShuffleCardBtn() {
     fetch("https://deckofcardsapi.com/api/deck/new/shuffle/")
         .then(res => res.json())
         .then(data => {
-            console.log(data)
+            // console.log(data)
             cardId = data.deck_id
         })
 }
@@ -20,7 +20,7 @@ function handleClickOnDrawCardsBtn(){
     fetch(`https://deckofcardsapi.com/api/deck/${cardId}/draw/?count=2`)
     .then(res => res.json())
     .then(data => {
-        console.log(data)
+        // console.log(data)
         cardsImage.children[0].innerHTML = `
         <img src=${data.cards[0].image} />
     `
@@ -31,3 +31,30 @@ function handleClickOnDrawCardsBtn(){
     
 }
 
+function determineWinner(card1, card2) {
+    const cardValueOptions = ["2", "3", "4", "5", "6", "7", "8", "9", 
+    "10", "JACK", "QUEEN", "KING", "ACE"]
+
+    const firstCardValueIndex = cardValueOptions.indexOf(card1.value)
+    const secondCardValueIndex = cardValueOptions.indexOf(card2.value)
+    console.log("card 1=", firstCardValueIndex)
+    console.log("card 2=", secondCardValueIndex)
+
+      
+    if (firstCardValueIndex > secondCardValueIndex) {
+        console.log("Card 1 wins!")
+    } else if (firstCardValueIndex < secondCardValueIndex) {
+        console.log("Card 2 wins!")
+    } else {
+        console.log("It's a tie!")
+    }
+}
+
+const card1Obj = {
+    value: "7"
+}
+const card2Obj = {
+    value: "KING"
+}
+
+determineWinner(card1Obj, card2Obj)
